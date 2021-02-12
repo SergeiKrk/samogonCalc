681039e29ef0d9666c5999a5f2e58374b50a1695
ksv <krjukov_s@mail.ru>
2021-02-12 12:48:36 +0400

    4 карточки

--- src/Components/CalcBlock.js
+++ src/Components/CalcBlock.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import razbavSam from '../assets/img/kalkulyator-razbavleniya-samogona-vodoj.png'
+import otborGolov from '../assets/img/kalkulyator-otbor-golov.png'
+import drobPeregon from '../assets/img/kalkulyator-drobnoj-peregonki.png'
 import absSpirt from '../assets/img/kalkulyator-absolyutnogo-spirta.png'
 
 const CalcBlock = () => {
@@ -19,14 +22,63 @@ const CalcBlock = () => {
                                 <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
                             </div>
                             <img 
+                                src={razbavSam}
+                                className="img-fluid"
+                                alt="калькулятор абсолютного спирта"
+                            />
+                        </div>
+                        <a className="text-secondary" href="#">
+                            <h3 className="text-secondary" className="text-center">Калькулятор разбавления самогона водой</h3>
+                        </a>
+                        <p>Необходимо смешать самогон с водой? Я помогу смешать в нужных пропорциях</p>
+                    </div>
+                    <div className="col-md-6 col-lg-3 mb-5">
+                        <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
+                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img 
+                                src={otborGolov}
+                                className="img-fluid"
+                                alt="калькулятор абсолютного спирта"
+                            />
+                        </div>
+                        <a className="text-secondary" href="#">
+                            <h3 className="text-secondary" className="text-center">Калькулятор отбор голов</h3>
+                        </a>
+                        <p className="text-center">Рассчитает объем чистого спирта в полученном и объем вредных «голов» в спирте первой перегонки.</p>
+                    </div>
+                    <div className="col-md-6 col-lg-3 mb-5">
+                        <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
+                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img 
+                                src={drobPeregon}
+                                className="img-fluid"
+                                alt="калькулятор абсолютного спирта"
+                            />
+                        </div>
+                        <a className="text-secondary" href="#">
+                            <h3 className="text-secondary" className="text-center">Калькулятор дробной перегонки спирта-сырца</h3>
+                        </a>
+                        <p className="text-center">Хотите начать дробную перегонку? Я помогу отобрать головы и хвосты!</p>
+                    </div>
+                    <div className="col-md-6 col-lg-3 mb-5">
+                        <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
+                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img 
                                 src={absSpirt}
                                 className="img-fluid"
                                 alt="калькулятор абсолютного спирта"
                             />
                         </div>
-                        <a src="#">
-                            <h3 className="text-center">Калькулятор абсолютного спирта</h3>
+                        <a className="text-secondary" href="#">
+                            <h3 className="text-secondary" className="text-center">Калькулятор абсолютного спирта</h3>
                         </a>
+                        <p className="text-center">Рассчитает объем чистого спирта в полученном и объем вредных «голов» в спирте первой перегонки.</p>
                     </div>
                 </div>
             </div>
