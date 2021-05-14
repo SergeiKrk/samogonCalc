353d72f73d75ecbbd2cf0403ece52f0e4d30468c
ksv <krjukov_s@mail.ru>
2021-05-14 12:57:12 +0400

    added Routing in Dialogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,40 +1,57 @@
 import React from 'react'
-import {Container, ListGroup, ListGroupItem} from 'react-bootstrap'
+import {Container} from 'react-bootstrap'
+import {BrowserRouter as Router, Switch, NavLink} from "react-router-dom"
 import Card from 'react-bootstrap/Card'
 import s from './Dialogs.module.css'
 import Massage from './Massage'
-import { FaCalculator } from "react-icons/fa"
+import {FaCalculator} from "react-icons/fa"
 
 const Dialogs = (props) => {
     return (
-        <div>
-            <Container>
-                <div className={`row`}>
-                    <div className={`col-md-3 text-left ${s.dialogs}`}>
-                        <Card className={`${s.card}`}>
-                            <Card.Body className={`${s.cardBody}`}>
-                                <Card.Title><FaCalculator /> Собеседники</Card.Title>
-                                <div className={`${s.dialogItems} ${s.active}`}>Yura</div>
-                                <div className={s.dialogItems}>Gosha</div>
-                                <div className={s.dialogItems}>Kolya</div>
-                            </Card.Body>
-                        </Card>
-                        <h3>Собеседники</h3>
-                        <div className={`${s.dialogItems} ${s.active}`}>Yura</div>
-                        <div className={s.dialogItems}>Gosha</div>
-                        <div className={s.dialogItems}>Kolya</div>
+        <Router>
+            <div>
+                <Container>
+                    <div className={`row`}>
+                        <div className={`col-md-4 text-left ${s.dialogs}`}>
+                            <Card className={`${s.card}`}>
+                                <Card.Body className={`${s.cardBody}`}>
+                                    <Card.Title>
+                                        <div className={`${s.icon}`}>
+                                            <FaCalculator size="21px"/>
+                                        </div>
+                                        Собеседники
+                                    </Card.Title>
+                                    <div className={s.dialogItemsBlock}>
+                                        <NavLink to="/dialogs/1" className={`${s.dialogItems} ${s.active}`}>Yura</NavLink>
+                                    </div>
+                                    <div className={s.dialogItemsBlock}>
+                                        <NavLink to="/dialogs/2" className={s.dialogItems}>Gosha</NavLink>
+                                    </div>
+                                    <div className={s.dialogItemsBlock}>
+                                        <NavLink to="/dialogs/3" className={s.dialogItems}>Kolya</NavLink>
+                                    </div>
+
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col-md-8">
+                            <Massage UserAva='https://samogoncalc.ru/img/Ava.jpg' UserName='Admin Tolya'
+                                     Massage='Hi everybody!'/>
+                            <Massage UserAva='https://samogoncalc.ru/img/Yura.jpg' UserName='Yura'
+                                     Massage='Ba-Ra-Ga-Be-De! Kergudu!'/>
+                            <Massage UserAva='https://samogoncalc.ru/img/Gosha.jpg' UserName='Gosha'
+                                     Massage='Where is Grandma?'/>
+                        </div>
+
+                        {/*<Switch>
+                        <Route exact path="dialogs/1" component={Yura}/>
+                        <Route path="dialogs/2" component={Gosha}/>
+                        <Route path="dialogs/3" component={Kolya}/>
+                    </Switch>*/}
                     </div>
-                    <div className="col-md-9">
-                        <Massage UserAva='https://samogoncalc.ru/img/Ava.jpg' UserName='Admin Tolya'
-                                 Massage='Hi everybody!'/>
-                        <Massage UserAva='https://samogoncalc.ru/img/Yura.jpg' UserName='Yura'
-                                 Massage='Ba-Ra-Ga-Be-De! Kergudu!'/>
-                        <Massage UserAva='https://samogoncalc.ru/img/Gosha.jpg' UserName='Gosha'
-                                 Massage='Where is Grandma?'/>
-                    </div>
-                </div>
-            </Container>
-        </div>
+                </Container>
+            </div>
+        </Router>
     )
 }
 
