4f861f80d7fdc950f0dee12014b7f433b48970ec
ksv <krjukov_s@mail.ru>
2021-05-24 13:44:27 +0400

    debug 'props' in 'Profile'

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -6,17 +6,8 @@ import MyPosts from "../Components/Profile/MyPosts/MyPosts"
 import ProfileInfo from "../Components/Profile/ProfileInfo/ProfileInfo"
 
 const Profile = (props) => {
-    /*let profileinfoData = [
-        {id: 1, img: 'https://samogoncalc.ru/img/Ava.jpg', title: 'Админ Евгений', description: 'Я тут админ!'}
-    ]
 
-    let postsData = [
-        {id: 1, userName: 'Админ Евгений', MyMassagePost: 'My first post My first post My first post My first post'},
-        {id: 2, userName: 'Админ Евгений', MyMassagePost: 'My twwfwv mfav;mda;flvm adfm;advmdav  adflmvfavmkfvfv'},
-        {id: 3, userName: 'Админ Евгений', MyMassagePost: 'My fadfvad fadvda afvadva'},
-    ]*/
-
-    let profileinfoElement = props.profileinfoData.map(profileinfo => <ProfileInfo Img={profileinfo.img} Title={profileinfo.title} Description={profileinfo.description}/>
+    let profileinfoElement = props.state.profileinfoData.map(profileinfo => <ProfileInfo Img={profileinfo.img} Title={profileinfo.title} Description={profileinfo.description}/>
     )
 
     return (
@@ -24,7 +15,7 @@ const Profile = (props) => {
             {profileinfoElement}
             <div className="container text-left">
                 <NewPost/>
-                <MyPosts postsData={props.postsData}/>
+                <MyPosts postsData={props.state.postsData}/>
             </div>
             <Footer/>
         </>
