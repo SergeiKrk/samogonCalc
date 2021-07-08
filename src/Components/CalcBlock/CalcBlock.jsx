9edcf51ce23cc81a9ee5a713c1b586029c08c5da
ksv <krjukov_s@mail.ru>
2021-07-08 13:28:00 +0400

    refactoring 'CalcBlock'

--- src/Components/CalcBlock/CalcBlock.jsx
+++ src/Components/CalcBlock/CalcBlock.jsx
@@ -6,6 +6,23 @@ import DisqusBlock from "../Header/DisqusBlock";
 
 const CalcBlock = (props) => {
 
+    let calcsData = [
+        {id: 1, href: 'kalkulyator-razbavleniya-samogona-vodoj', img: 'https://samogoncalc.ru/img/kalkulyator-razbavleniya-samogona-vodoj.png', title:'Калькулятор разбавления самогона водой', description: 'Необходимо смешать самогон с водой? Я помогу смешать в нужных пропорциях'},
+        {id: 2, href: 'kalkulyator-otbor-golov', img: 'https://samogoncalc.ru/img/kalkulyator-otbor-golov.png', title: 'Калькулятор отбор голов', description: 'Рассчитает объем чистого спирта в полученном и объем вредных «голов» в спирте первой перегонки.' },
+        {id: 3, href: 'kalkulyator-drobnoj-peregonki', img: 'https://samogoncalc.ru/img/kalkulyator-drobnoj-peregonki.png', title: 'Калькулятор дробной перегонки спирта-сырца', description: 'Хотите начать дробную перегонку? Я помогу отобрать головы и хвосты!' },
+        {id: 4, href: 'razbavlenie-samogona-vodoj-posle-pervoj-peregonki', img: 'https://samogoncalc.ru/img/razbavlenie-samogona-vodoj-posle-pervoj-peregonki.png', title: 'Калькулятор разбавления самогона водой (после первого перегона)', description: 'Необходимо смешать самогон с водой? Я помогу смешать в нужных пропорциях!' },
+        {id: 5, href: 'razbavlenie-samogona-vodoj-posle-vtorogo-peregona', img: 'https://samogoncalc.ru/img/razbavlenie-samogona-vodoj-posle-vtorogo-peregona.png', title: 'Калькулятор разбавления самогона водой (после второго перегона)', description: 'Необходимо смешать самогон с водой? Я помогу смешать в нужных пропорциях!' },
+        {id: 6, href: 'kalkulyator-smeshivaniya-spirtov', img: 'https://samogoncalc.ru/img/kalkulyator-smeshivaniya-spirtov.png', title: 'Калькулятор смешивания спиртов', description: 'Смешиваете две разных крепости? Узнайте сейчас какой градус будет на выходе!' },
+        {id: 7, href: 'kalkulyator-absolyutnogo-spirta', img: 'https://samogoncalc.ru/img/kalkulyator-absolyutnogo-spirta.png', title: 'Калькулятор абсолютного спирта', description: 'Рассчитает объем чистого спирта в полученном и объем вредных «голов» в спирте первой перегонки.' },
+        {id: 8, href: 'kalkulyator-sebestoimosti-samogona', img: 'https://samogoncalc.ru/img/kalkulyator-sebestoimosti-samogona.png', title: 'Калькулятор себестоимости самогона', description: 'Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла.' },
+        {id: 9, href: 'kalkulyator-saharnoj-bragi', img: 'https://samogoncalc.ru/img/kalkulyator-saharnoj-bragi.png', title: 'Калькулятор сахарной браги', description: 'Вычисляет крепость браги и оптимальные пропорции сахара и воды для браги.' },
+        {id: 10, href: 'kalkulyator-vodki-iz-spirta', img: 'https://samogoncalc.ru/img/kalkulyator-vodki-iz-spirta.png', title: 'Калькулятор водки из спирта', description: 'Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла.' },
+        {id: 11, href: 'kalkulyator-spirta-ot-temperatury', img: 'https://samogoncalc.ru/img/kalkulyator-spirta-ot-temperatury.png', title: 'Калькулятор спирта от температуры', description: 'Рассчитает реальную крепость самогона при температуре больше или меньше 20 °C.' },
+        {id: 12, href: 'kalkulyator-zameny-sahara-glyukozoj', img: 'https://samogoncalc.ru/img/kalkulyator-zameny-sahara-glyukozoj.png', title: 'Калькулятор замены сахара глюкозой', description: 'Определяет насколько меньше потребуется глюкозы, чтобы выход спирта из браги был как с 1 кг обычного сахара.' }
+    ]
+
+    let calcsElements = calcsData.map(calc => <CalcCard Href='kalkulyator-razbavleniya-samogona-vodoj' Img={calc.img} Title={calc.title} Description={calc.description} />)
+
     return(
         <section className="page-section portfolio" id="portfolio">
             <div className={s.FullContainer}>
@@ -16,18 +33,7 @@ const CalcBlock = (props) => {
                     <div className="divider-custom-line"></div>
                 </div>
                 <div className="row justify-content-center">
-                    <CalcCard Href='kalkulyator-razbavleniya-samogona-vodoj' Img="https://samogoncalc.ru/img/kalkulyator-razbavleniya-samogona-vodoj.png" Title='Калькулятор разбавления самогона водой' Description='Необходимо смешать самогон с водой? Я помогу смешать в нужных пропорциях' />
-                    <CalcCard Href='kalkulyator-otbor-golov' Img="https://samogoncalc.ru/img/kalkulyator-otbor-golov.png" Title='Калькулятор отбор голов' Description='Рассчитает объем чистого спирта в полученном и объем вредных «голов» в спирте первой перегонки.' />
-                    <CalcCard Href='kalkulyator-drobnoj-peregonki' Img="https://samogoncalc.ru/img/kalkulyator-drobnoj-peregonki.png" Title='Калькулятор дробной перегонки спирта-сырца' Description= 'Хотите начать дробную перегонку? Я помогу отобрать головы и хвосты!' />
-                    <CalcCard Href='razbavlenie-samogona-vodoj-posle-pervoj-peregonki' Img="https://samogoncalc.ru/img/razbavlenie-samogona-vodoj-posle-pervoj-peregonki.png" Title='Калькулятор разбавления самогона водой (после первого перегона)' Description= 'Необходимо смешать самогон с водой? Я помогу смешать в нужных пропорциях!' />
-                    <CalcCard Href='razbavlenie-samogona-vodoj-posle-vtorogo-peregona' Img="https://samogoncalc.ru/img/razbavlenie-samogona-vodoj-posle-vtorogo-peregona.png" Title='Калькулятор разбавления самогона водой (после второго перегона)' Description= 'Необходимо смешать самогон с водой? Я помогу смешать в нужных пропорциях!' />
-                    <CalcCard Href='kalkulyator-smeshivaniya-spirtov' Img="https://samogoncalc.ru/img/kalkulyator-smeshivaniya-spirtov.png" Title='Калькулятор смешивания спиртов' Description= 'Смешиваете две разных крепости? Узнайте сейчас какой градус будет на выходе!' />
-                    <CalcCard Href='kalkulyator-absolyutnogo-spirta' Img="https://samogoncalc.ru/img/kalkulyator-absolyutnogo-spirta.png" Title='Калькулятор абсолютного спирта' Description= 'Рассчитает объем чистого спирта в полученном и объем вредных «голов» в спирте первой перегонки.' />
-                    <CalcCard Href='kalkulyator-sebestoimosti-samogona' Img="https://samogoncalc.ru/img/kalkulyator-sebestoimosti-samogona.png" Title='Калькулятор себестоимости самогона' Description= 'Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла.' />
-                    <CalcCard Href='kalkulyator-saharnoj-bragi' Img="https://samogoncalc.ru/img/kalkulyator-saharnoj-bragi.png" Title='Калькулятор сахарной браги' Description= 'Вычисляет крепость браги и оптимальные пропорции сахара и воды для браги.' />
-                    <CalcCard Href='kalkulyator-vodki-iz-spirta' Img="https://samogoncalc.ru/img/kalkulyator-vodki-iz-spirta.png" Title='Калькулятор водки из спирта' Description= 'Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла Бла бла бла.' />
-                    <CalcCard Href='kalkulyator-spirta-ot-temperatury' Img="https://samogoncalc.ru/img/kalkulyator-spirta-ot-temperatury.png" Title='Калькулятор спирта от температуры' Description= 'Рассчитает реальную крепость самогона при температуре больше или меньше 20 °C.' />
-                    <CalcCard Href='kalkulyator-zameny-sahara-glyukozoj' Img="https://samogoncalc.ru/img/kalkulyator-zameny-sahara-glyukozoj.png" Title='Калькулятор замены сахара глюкозой' Description= 'Определяет насколько меньше потребуется глюкозы, чтобы выход спирта из браги был как с 1 кг обычного сахара.' />
+                    { calcsElements }
                 </div>
             </div>
             <DisqusBlock />
