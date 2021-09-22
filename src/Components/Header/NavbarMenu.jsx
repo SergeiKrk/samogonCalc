092a450cc9e5d43c39843b7a733fb9efa9806a67
ksv <krjukov_s@mail.ru>
2021-09-22 15:20:02 +0400

    Install 'react-router-bootstrap' + refactoring 'NavbarMenu.jsx'

--- src/Components/Header/NavbarMenu.jsx
+++ src/Components/Header/NavbarMenu.jsx
@@ -1,53 +1,92 @@
-import React from 'react'
-import {
-    Container,
-    Navbar,
-    Nav,
-    NavDropdown
-} from 'react-bootstrap'
+import React from "react";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const NavbarMenu = () => {
-    return (
-        <>
-            <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Container><Navbar.Brand href="/">SamagonCalc</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                            <NavDropdown title="Все калькуляторы" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="/kalkulyator-razbavleniya-samogona-vodoj">Калькулятор
-                                    разбавления самогона водой</NavDropdown.Item>
-                                <NavDropdown.Item href="/kalkulyator-otbor-golov">Калькулятор отбор
-                                    голов</NavDropdown.Item>
-                                <NavDropdown.Item href="/kalkulyator-drobnoj-peregonki">Калькулятор дробной
-                                    перегонки</NavDropdown.Item>
-                                <NavDropdown.Item href="/razbavlenie-samogona-vodoj-posle-pervoj-peregonki">Калькулятор
-                                    разбавления самогона (после I перегона)</NavDropdown.Item>
-                                <NavDropdown.Item href="/razbavlenie-samogona-vodoj-posle-vtorogo-peregona">Калькулятор
-                                    разбавления самогона (после II перегона)</NavDropdown.Item>
-                                <NavDropdown.Item href="/kalkulyator-absolyutnogo-spirta">Калькулятор абсолютного
-                                    спирта</NavDropdown.Item>
-                                <NavDropdown.Item href="/kalkulyator-smeshivaniya-spirtov">Калькулятор смешивания
-                                    спиртов</NavDropdown.Item>
-                                <NavDropdown.Item href="/kalkulyator-sebestoimosti-samogona">Калькулятор себестоимости
-                                    самогона</NavDropdown.Item>
-                                <NavDropdown.Item href="/kalkulyator-saharnoj-bragi">Калькулятор сахарной
-                                    браги</NavDropdown.Item>
-                                <NavDropdown.Item href="/kalkulyator-vodki-iz-spirta">Калькулятор водки из
-                                    спирта</NavDropdown.Item>
-                                <NavDropdown.Item href="/kalkulyator-spirta-ot-temperatury">Калькулятор спирта от
-                                    температуры</NavDropdown.Item>
-                                <NavDropdown.Item href="/kalkulyator-zameny-sahara-glyukozoj">Калькулятор замены сахара
-                                    глюкозой</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link href="/about">О проекте</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse></Container>
-            </Navbar>
-        </>
-    );
-}
+  return (
+    <>
+      <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>SamagonCalc</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <NavDropdown
+                title="Все калькуляторы"
+                id="collasible-nav-dropdown"
+              >
+                <LinkContainer to="/kalkulyator-razbavleniya-samogona-vodoj">
+                  <NavDropdown.Item>
+                    Калькулятор разбавления самогона водой
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/kalkulyator-otbor-golov">
+                  <NavDropdown.Item>Калькулятор отбор голов</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/kalkulyator-drobnoj-peregonki">
+                  <NavDropdown.Item>
+                    Калькулятор дробной перегонки
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/razbavlenie-samogona-vodoj-posle-pervoj-peregonki">
+                  <NavDropdown.Item>
+                    Калькулятор разбавления самогона (после I перегона)
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/razbavlenie-samogona-vodoj-posle-vtorogo-peregona">
+                  <NavDropdown.Item>
+                    Калькулятор разбавления самогона (после II перегона)
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/kalkulyator-absolyutnogo-spirta">
+                  <NavDropdown.Item>
+                    Калькулятор абсолютного спирта
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/kalkulyator-smeshivaniya-spirtov">
+                  <NavDropdown.Item>
+                    Калькулятор смешивания спиртов
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/kalkulyator-sebestoimosti-samogona">
+                  <NavDropdown.Item>
+                    Калькулятор себестоимости самогона
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/kalkulyator-saharnoj-bragi">
+                  <NavDropdown.Item>
+                    Калькулятор сахарной браги
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/kalkulyator-vodki-iz-spirta">
+                  <NavDropdown.Item>
+                    Калькулятор водки из спирта
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/kalkulyator-spirta-ot-temperatury">
+                  <NavDropdown.Item>
+                    Калькулятор спирта от температуры
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/kalkulyator-zameny-sahara-glyukozoj">
+                  <NavDropdown.Item>
+                    Калькулятор замены сахара глюкозой
+                  </NavDropdown.Item>
+                </LinkContainer>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <LinkContainer to="/about">
+                <Nav.Link>О проекте</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
 export default NavbarMenu;
