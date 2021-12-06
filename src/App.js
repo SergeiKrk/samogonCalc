367b28a8c9c06586af4b08544de5faf0af4a3b58
ksv <krjukov_s@mail.ru>
2021-12-06 17:39:43 +0400

    Create hoc 'withSuspense' and refactoring 'App.js' - added React.lazy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import store from "./redux/redux-store";
+import { withSuspense } from "./hoc/withSuspense";
 import NavbarMenu from "./Components/Header/NavbarMenu";
 import Home from "./Pages/Home/Home";
-import AbsolyutnijSpirt from "./Pages/AbsolyutnijSpirt";
-import DrobnayaPeregonka from "./Pages/DrobnayaPeregonka";
-import RazbavleniyaSamogona from "./Pages/RazbavleniyaSamogona";
-import OtborGolov from "./Pages/OtborGolov";
-import PoslePervoj from "./Pages/PoslePervoj";
-import PosleVtoroj from "./Pages/PosleVtoroj";
-import SmeshSpirt from "./Pages/SmeshSpirt";
-import SebestoimSamogona from "./Pages/SebestoimSamogona";
-import SebestoimApprox from "./Pages/SebestoimApprox";
-import SaharBragi from "./Pages/SaharBragi";
-import Vodka from "./Pages/Vodka";
-import SpirtaTemper from "./Pages/SpirtaTemper";
-import ZamenyGlyukozoj from "./Pages/ZamenyGlyukozoj";
-import About from "./Pages/About";
-import NoMatch404 from "./Pages/NoMatch404";
-import store from "./redux/redux-store";
+
+const AbsolyutnijSpirt = React.lazy(() => import("./Pages/AbsolyutnijSpirt"));
+const DrobnayaPeregonka = React.lazy(() => import("./Pages/DrobnayaPeregonka"));
+const RazbavleniyaSamogona = React.lazy(() =>
+  import("./Pages/RazbavleniyaSamogona")
+);
+const OtborGolov = React.lazy(() => import("./Pages/OtborGolov"));
+const PoslePervoj = React.lazy(() => import("./Pages/PoslePervoj"));
+const PosleVtoroj = React.lazy(() => import("./Pages/PosleVtoroj"));
+const SmeshSpirt = React.lazy(() => import("./Pages/SmeshSpirt"));
+const SebestoimSamogona = React.lazy(() => import("./Pages/SebestoimSamogona"));
+const SebestoimApprox = React.lazy(() => import("./Pages/SebestoimApprox"));
+const SaharBragi = React.lazy(() => import("./Pages/SaharBragi"));
+const Vodka = React.lazy(() => import("./Pages/Vodka"));
+const SpirtaTemper = React.lazy(() => import("./Pages/SpirtaTemper"));
+const ZamenyGlyukozoj = React.lazy(() => import("./Pages/ZamenyGlyukozoj"));
+const About = React.lazy(() => import("./Pages/About"));
+const NoMatch404 = React.lazy(() => import("./Pages/NoMatch404"));
 
 const App = (props) => {
   //
@@ -31,58 +36,58 @@ const App = (props) => {
           <Route exact path="/" render={() => <Home store={store} />} />
           <Route
             path="/kalkulyator-razbavleniya-samogona-vodoj"
-            render={() => <RazbavleniyaSamogona store={store} />}
+            render={withSuspense(RazbavleniyaSamogona)}
           />
           <Route
             path="/kalkulyator-otbor-golov"
-            render={() => <OtborGolov store={store} />}
+            render={withSuspense(OtborGolov)}
           />
           <Route
             path="/kalkulyator-drobnoj-peregonki"
-            render={() => <DrobnayaPeregonka store={store} />}
+            render={withSuspense(DrobnayaPeregonka)}
           />
           <Route
             path="/razbavlenie-samogona-vodoj-posle-pervoj-peregonki"
-            render={() => <PoslePervoj store={store} />}
+            render={withSuspense(PoslePervoj)}
           />
           <Route
             path="/razbavlenie-samogona-vodoj-posle-vtorogo-peregona"
-            render={() => <PosleVtoroj store={store} />}
+            render={withSuspense(PosleVtoroj)}
           />
           <Route
             path="/kalkulyator-absolyutnogo-spirta"
-            render={() => <AbsolyutnijSpirt store={store} />}
+            render={withSuspense(AbsolyutnijSpirt)}
           />
           <Route
             path="/kalkulyator-smeshivaniya-spirtov"
-            render={() => <SmeshSpirt store={store} />}
+            render={withSuspense(SmeshSpirt)}
           />
           <Route
             path="/kalkulyator-sebestoimosti-samogona"
-            render={() => <SebestoimSamogona store={store} />}
+            render={withSuspense(SebestoimSamogona)}
           />
           <Route
             path="/primernaya-stoimost-samogona"
-            render={() => <SebestoimApprox store={store} />}
+            render={withSuspense(SebestoimApprox)}
           />
           <Route
             path="/kalkulyator-saharnoj-bragi"
-            render={() => <SaharBragi store={store} />}
+            render={withSuspense(SaharBragi)}
           />
           <Route
             path="/kalkulyator-vodki-iz-spirta"
-            render={() => <Vodka store={store} />}
+            render={withSuspense(Vodka)}
           />
           <Route
             path="/kalkulyator-spirta-ot-temperatury"
-            render={() => <SpirtaTemper store={store} />}
+            render={withSuspense(SpirtaTemper)}
           />
           <Route
             path="/kalkulyator-zameny-sahara-glyukozoj"
-            render={() => <ZamenyGlyukozoj store={store} />}
+            render={withSuspense(ZamenyGlyukozoj)}
           />
-          <Route path="/about" render={() => <About />} />
-          <Route path="*" render={() => <NoMatch404 store={store} />} />
+          <Route path="/about" render={withSuspense(About)} />
+          <Route path="*" render={withSuspense(NoMatch404)} />
         </Switch>
       </Router>
     </div>
--- src/Components/Breadcrumbs/Breadcrumbs.jsx
+++ src/Components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,18 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import React from "react";
 
 const Breadcrumbs = (props) => {
-    return (
-        <div className="container">
-            <Breadcrumb className="text-center">
-                <Breadcrumb.Item href="/">Калькуляторы самогонщика</Breadcrumb.Item>
-                <Breadcrumb.Item href={props.Breadcrumbs.Href}>
-                    {props.Breadcrumbs.Title}
-                </Breadcrumb.Item>
-            </Breadcrumb>
-        </div>
-    );
-}
+  return (
+    <div className="container">
+      <Breadcrumb className="text-center">
+        <Breadcrumb.Item href="/">Калькуляторы самогонщика</Breadcrumb.Item>
+        <Breadcrumb.Item href={props.Breadcrumbs.Href}>
+          {props.Breadcrumbs.Title}
+        </Breadcrumb.Item>
+      </Breadcrumb>
+    </div>
+  );
+};
 
 export default Breadcrumbs;
