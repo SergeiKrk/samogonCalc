5f4e52be040f256eaa2462b37ac47587ffe54e33
ksv <krjukov_s@mail.ru>
2021-12-07 17:22:56 +0400

    Refactoring 'Footer'

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,34 +7,26 @@ const Footer = () => {
         <div className="container">
           <div className="row">
             <div className="col-lg-4 mb-5 mb-lg-0">
-              <h4 className="text-uppercase mb-4">Location</h4>
+              <h4 className="text-uppercase mb-4">SamogonCalc</h4>
               <p className="lead mb-0">
-                2215 John Daniel Drive
-                <br />
-                Clark, MO 65243
+                - набор быстрых онлайн-калькулятороов для винокуров и
+                самогонщиков
               </p>
             </div>
             <div className="col-lg-4 mb-5 mb-lg-0">
-              <h4 className="text-uppercase mb-4">Around the Web</h4>
-              <a className="btn btn-outline-light btn-social mx-1" href="#!">
-                <i className="fab fa-fw fa-facebook-f"></i>
-              </a>
-              <a className="btn btn-outline-light btn-social mx-1" href="#!">
-                <i className="fab fa-fw fa-twitter"></i>
-              </a>
-              <a className="btn btn-outline-light btn-social mx-1" href="#!">
-                <i className="fab fa-fw fa-linkedin-in"></i>
-              </a>
-              <a className="btn btn-outline-light btn-social mx-1" href="#!">
-                <i className="fab fa-fw fa-dribbble"></i>
-              </a>
+              <img
+                src="https://samogoncalc.ru/img/kalkulyator-samogonshchika.png"
+                width="150"
+                className="img-fluid"
+                alt="калькулятор самогонщика Samogoncalc"
+              />
             </div>
             <div className="col-lg-4">
-              <h4 className="text-uppercase mb-4">About Freelancer</h4>
+              <h4 className="text-uppercase mb-4">Контакты</h4>
               <p className="lead mb-0">
-                Freelance is a free to use, MIT licensed Bootstrap theme created
-                by
-                <a href="http://startbootstrap.com">Start Bootstrap</a>.
+                По всем вопросам пишите в Telegram <br />
+                Sergei Krk{" "}
+                <a href="https://telegram.me/sergeikrk">@sergeikrk</a>
               </p>
             </div>
           </div>
@@ -43,74 +35,6 @@ const Footer = () => {
       <div className="copyright py-4 text-center text-white">
         <div className="container">
           <small>SamogonCalc.ru © 2021</small>
-        </div>
-      </div>
-      <div className="scroll-to-top d-lg-none position-fixed">
-        <a
-          className="js-scroll-trigger d-block text-center text-white rounded"
-          href="#page-top"
-        >
-          <i className="fa fa-chevron-up"></i>
-        </a>
-      </div>
-      <div
-        className="portfolio-modal modal fade"
-        id="portfolioModal1"
-        tablndex="-1"
-        role="dialog"
-        aria-labelledby="portfolioModal1Label"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-xl" role="document">
-          <div className="modal-content">
-            <button
-              className="close"
-              type="button"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">
-                <i className="fas fa-times"></i>
-              </span>
-            </button>
-            <div className="modal-body text-center">
-              <div className="container">
-                <div className="row justify-content-center">
-                  <div className="col-lg-8">
-                    <h2
-                      className="portfolio-modal-title text-secondary text-uppercase mb-0"
-                      id="portfolioModal1Label"
-                    >
-                      Log Cabin
-                    </h2>
-                    <div className="divider-custom">
-                      <div className="divider-custom-line"></div>
-                      <div className="divider-custom-icon">
-                        <i className="fas fa-star"></i>
-                      </div>
-                      <div className="divider-custom-line"></div>
-                    </div>
-                    <img
-                      className="img-fluid rounded mb-5"
-                      src="assets/img/portfolio/cabin.png"
-                      alt=""
-                    />
-                    <p className="mb-5">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Mollitia neque assumenda ipsam nihil, molestias magnam,
-                      recusandae quos quis inventore quisquam velit asperiores,
-                      vitae? Reprehenderit soluta, eos quod consequuntur itaque.
-                      Nam.
-                    </p>
-                    <button className="btn btn-primary" data-dismiss="modal">
-                      <i className="fas fa-times fa-fw"></i>
-                      Close Window
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
